18a9d9c6d6625d6fd98a96a57ed1fde4c29d09d1
Liam <jacox0952@gmail.com>
2023-08-30 09:47:22 -0500

    Initial Commit

---
+++ creativeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  creativeApp
+//
+//  Created by WILLIAM COX on 8/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
