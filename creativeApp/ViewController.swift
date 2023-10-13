e07884e77baa1940eb19e39d53f55112951b55df
Liam Cox <william.cox24@d155.org>
2023-10-13 09:09:31 -0500

    first commit

--- creativeApp/ViewController.swift
+++ creativeApp/ViewController.swift
@@ -5,15 +5,66 @@
 //  Created by WILLIAM COX on 8/30/23.
 //
 
+protocol newStudent {
+    func addStudent()
+}
+
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    var students : [Student] = []
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        students.append(Student(name: "Brian", age: 17, liamRating: grade.B))
+        students.append(Student(name: "Andrew", age: 17, liamRating: grade.A))
+        students.append(Student(name: "Stanislov", age: 17, liamRating: grade.A))
+        students.append(Student(name: "Miles", age: 17, liamRating: grade.B))
     }
 
 
+    @IBAction func toQuiz(_ sender: Any) {
+        performSegue(withIdentifier: "toQuiz", sender: self)
+    }
+    
+    @IBAction func toView(_ sender: Any) {
+        performSegue(withIdentifier: "toView", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc = segue.destination as! ViewControllerView
+        nvc.students1 = students
+
+    }
+    
+    
+    
 }
 
+enum grade {
+    case A
+    case B
+    case C
+    case D
+    case F
+}
+
+class Student {
+    var name: String
+    var age: Int
+    var liamRating : grade
+    
+    init(name: String, age: Int, liamRating: grade) {
+        self.name = name
+        self.age = age
+        self.liamRating = liamRating
+    }
+}
---
+++ creativeApp/ViewControllerView.swift
@@ -0,0 +1,84 @@
+//
+//  ViewControllerView.swift
+//  creativeApp
+//
+//  Created by WILLIAM COX on 10/12/23.
+//
+
+import UIKit
+
+class ViewControllerView: UIViewController {
+
+    weak var delegate: ViewController!
+    
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var ageInput: UITextField!
+    @IBOutlet weak var ratingInput: UITextField!
+    
+    var students1 : [Student] = []
+    var spot : Int = 0
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    //    nvc.delegate = self
+        // Do any additional setup after loading the view.
+        nameLabel.text = students1[0].name
+        ageLabel.text = "\(students1[0].age)"
+        ratingLabel.text = "\(students1[0].liamRating)"
+    }
+    
+    @IBAction func nextStudent(_ sender: Any) {
+        if spot < students1.count-1 {
+            spot = spot + 1
+            nameLabel.text = students1[spot].name
+            ageLabel.text = "\(students1[spot].age)"
+           ratingLabel.text = "\(students1[spot].liamRating)"
+    
+        } else {
+            spot = 0
+            nameLabel.text = students1[spot].name
+            ageLabel.text = "\(students1[spot].age)"
+            
+        }
+    }
+    
+    @IBAction func previousStudent(_ sender: Any) {
+        
+        if spot > 0 {
+            spot = spot - 1
+            nameLabel.text = students1[spot].name
+            ageLabel.text = "\(students1[spot].age)"
+            ratingLabel.text = "\(students1[spot].liamRating)"
+        } else {
+            spot = students1.count-1
+            nameLabel.text = students1[spot].name
+            ageLabel.text = "\(students1[spot].age)"
+            ratingLabel.text = "\(students1[spot].liamRating)"
+        }
+        
+    }
+    
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+        let studentName = String(nameInput.text!)
+        let studentAge = Int(ageInput.text!)
+        let studentGrade = String(ratingInput.text!)
+        
+        if studentGrade == "A" {
+            
+        } else if studentGrade == "B" {
+            
+        } else if studentGrade == "C" {
+            
+        } else if studentGrade == "D" {
+            
+        } else {
+            
+        }
+    }
+    
+    
+}
